ea72f3e3fd7368926c9af54ef7aa614ae1d8718f
Suyi Thonatos.Yang <thonatos.yang@gmail.com>
2017-09-25 21:00:50 +0800

    feat: show image for services

--- src/views/clusters/components/serviceTalbe.js
+++ src/views/clusters/components/serviceTalbe.js
@@ -2,17 +2,19 @@ import React from 'react'
 import { Table, Tag } from 'antd'
 import moment from 'moment'
 
-const TAG_COLORS = ['', '#87d068', '#2db7f5', '#f50', '#108ee9', 'orange']
+const TAG_COLORS = [ '', '#87d068', '#2db7f5', '#f50', '#108ee9', 'orange' ]
 
-const firstUpperCase = ([first, ...rest]) => first.toUpperCase() + rest.join('')
+const firstUpperCase = ([ first, ...rest ]) => first.toUpperCase() + rest.join('')
 
-const expandedRowRender = ({ containers }) => {
+const expandedRowRender = ({ containers, definition }) => {
   const keys = Object.keys(containers)
+  const { image } = definition;
   return (
     <div>
+      <Tag>{image}</Tag>
       <ul>
         {keys.map(k => {
-          const { name, ip, health, status, vm_id, fail_count } = containers[k]
+          const { name, ip, health, status, vm_id, fail_count } = containers[ k ]
           const tags = {
             name,
             status,
@@ -23,17 +25,17 @@ const expandedRowRender = ({ containers }) => {
           }
           return (
             <li key={k} style={{ marginTop: '0.8em' }}>
-              <p>
+              <div>
                 {Object.keys(tags).map((k, i) => {
                   const name = firstUpperCase(k)
-                  const record = tags[k]
+                  const record = tags[ k ]
                   return (
-                    <Tag color={TAG_COLORS[i]}>
+                    <Tag color={TAG_COLORS[ i ]} key={i}>
                       {name}: {record}
                     </Tag>
                   )
                 })}
-              </p>
+              </div>
             </li>
           )
         })}
@@ -45,27 +47,27 @@ const expandedRowRender = ({ containers }) => {
 const ServiceTable = ({ title, data, loading, langs }) => {
   const columns = [
     {
-      title: langs['service_name'],
+      title: langs[ 'service_name' ],
       dataIndex: 'name',
       key: 'name',
     },
     {
-      title: langs['service_project'],
+      title: langs[ 'service_project' ],
       dataIndex: 'project',
       key: 'project',
     },
     {
-      title: langs['service_desired_state'],
+      title: langs[ 'service_desired_state' ],
       dataIndex: 'desired_state',
       key: 'desired_state',
     },
     {
-      title: langs['service_current_state'],
+      title: langs[ 'service_current_state' ],
       dataIndex: 'current_state',
       key: 'current_state',
     },
     {
-      title: langs['service_updated'],
+      title: langs[ 'service_updated' ],
       dataIndex: 'updated',
       key: 'updated',
       render: text => {
@@ -84,9 +86,9 @@ const ServiceTable = ({ title, data, loading, langs }) => {
       title={
         title
           ? () =>
-              <p>
-                {langs[title]}
-              </p>
+            <p>
+              {langs[ title ]}
+            </p>
           : null
       }
       style={{ marginBottom: '1em' }}
